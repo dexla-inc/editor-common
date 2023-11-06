5ff09f68575ed8053132b4d12cec92106ede88f9
Tom McDonough <tom@dexla.ai>
2023-11-06 13:42:55 +0000

    Update ThemeModel.cs

--- src/Dexla.Common.Editor/Models/ThemeModel.cs
+++ src/Dexla.Common.Editor/Models/ThemeModel.cs
@@ -47,4 +47,110 @@ public class ThemeModel : IModelWithUserId
     {
         WebsiteUrl = WebsiteUrl?.TrimEnd('/');
     }
+
+    const string DefaultFontFamily = "Open Sans";
+
+    public static ThemeModel GetDefault()
+    {
+        return new ThemeModel
+        {
+            Fonts = new List<FontDto>
+            {
+                new()
+                {
+                    FontFamily = DefaultFontFamily,
+                    Tag = "H1",
+                    FontWeight = "700",
+                    FontSize = "3.052rem",
+                    LineHeight = "1.2",
+                    LetterSpacing = "0.5px"
+                },
+                new()
+                {
+                    FontFamily = DefaultFontFamily,
+                    Tag = "H2",
+                    FontWeight = "700",
+                    FontSize = "2.441rem",
+                    LineHeight = "1.2",
+                    LetterSpacing = "0.4px"
+                },
+                new()
+                {
+                    FontFamily = DefaultFontFamily,
+                    Tag = "H3",
+                    FontWeight = "700",
+                    FontSize = "1.953rem",
+                    LineHeight = "1.2",
+                    LetterSpacing = "0.3px"
+                },
+                new()
+                {
+                    FontFamily = DefaultFontFamily,
+                    Tag = "H4",
+                    FontWeight = "700",
+                    FontSize = "1.563rem",
+                    LineHeight = "1.2",
+                    LetterSpacing = "0.2px"
+                },
+                new()
+                {
+                    FontFamily = DefaultFontFamily,
+                    Tag = "H5",
+                    FontWeight = "700",
+                    FontSize = "1.25rem",
+                    LineHeight = "1.2",
+                    LetterSpacing = "0.1px"
+                },
+                new()
+                {
+                    FontFamily = DefaultFontFamily,
+                    Tag = "H6",
+                    FontWeight = "700",
+                    FontSize = "1rem",
+                    LineHeight = "1.2",
+                    LetterSpacing = "0.1px"
+                },
+                new()
+                {
+                    FontFamily = DefaultFontFamily,
+                    Tag = "P",
+                    FontWeight = "400",
+                    FontSize = "0.8rem",
+                    LineHeight = "1.5",
+                    LetterSpacing = "0.5px"
+                }
+            },
+            Colors = new List<ColorDto>
+            {
+                new() { Hex = "#2F65CB", Name = "Primary", IsDefault = true, FriendlyName = "Primary", },
+                new() { Hex = "#D9D9D9", Name = "Secondary", IsDefault = true, FriendlyName = "Secondary" },
+                new() { Hex = "#FFFFFF", Name = "PrimaryText", IsDefault = true, FriendlyName = "Primary Text", },
+                new() { Hex = "#000000", Name = "SecondaryText", IsDefault = true, FriendlyName = "Secondary Text" },
+                new() { Hex = "#E57F4F", Name = "Tertiary", IsDefault = true, FriendlyName = "Tertiary" },
+                new() { Hex = "#FFFFFF", Name = "TertiaryText", IsDefault = true, FriendlyName = "Tertiary Text" },
+                new() { Hex = "#FF9600", Name = "Background", IsDefault = true, FriendlyName = "Background" },
+                new() { Hex = "#FE191C", Name = "Danger", IsDefault = true, FriendlyName = "Danger" },
+                new() { Hex = "#FFCC00", Name = "Warning", IsDefault = true, FriendlyName = "Warning" },
+                new() { Hex = "#10D48E", Name = "Success", IsDefault = true, FriendlyName = "Success" },
+                new() { Hex = "#F1F1F1", Name = "Neutral", IsDefault = true, FriendlyName = "Neutral" },
+                new() { Hex = "#000000", Name = "Black", IsDefault = true, FriendlyName = "Black" },
+                new() { Hex = "#FFFFFF", Name = "White", IsDefault = true, FriendlyName = "White" },
+                new() { Hex = "#EEEEEE", Name = "Border", IsDefault = true, FriendlyName = "Border" }
+            },
+            ResponsiveBreakpoints = new List<ResponsiveBreakpointDto>
+            {
+                new() { Breakpoint = "100%", Type = "Desktop" },
+                new() { Breakpoint = "768px", Type = "Tablet" },
+                new() { Breakpoint = "480px", Type = "Mobile" }
+            },
+            DefaultFont = DefaultFontFamily,
+            HasCompactButtons = true,
+            DefaultRadius = "sm",
+            DefaultSpacing = "md",
+            Loader = LoaderTypes.OVAL.ToString(),
+            FocusRing = FocusRingTypes.DEFAULT.ToString(),
+            FaviconUrl = "https://mortenjonassen.dk/wp-content/uploads/2020/02/Per-larsen-favicon.jpg",
+            LogoUrl = "https://wwwspennarecom.cdn.triggerfish.cloud/uploads/2015/08/Your-Logo-Here-Black-22.jpg",
+        };
+    }
 }
