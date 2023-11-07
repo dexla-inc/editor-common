c0ad6669907ee42f8dd7a66f882f7a4392b3d213
Tom McDonough <tom@dexla.ai>
2023-11-07 13:56:22 +0000

    Updated queue response to include call back url

--- src/Dexla.Common.Editor/Responses/QueueResponse.cs
+++ src/Dexla.Common.Editor/Responses/QueueResponse.cs
@@ -4,27 +4,29 @@ namespace Dexla.Common.Editor.Responses;
 
 public class QueueResponse : ISuccess
 {
-    public string Message { get; set; } = "Your request has been accepted for processing.";
+    public string Message { get; } = "Your request has been accepted for processing.";
     public string CallbackUrl { get; set; } = string.Empty;
     public string StatusEndpoint { get; set; } = string.Empty;
     public long EstimatedTimeOfCompletion { get; set; }
-    public Dictionary<string, string> ExpectedCallback { get; set; } = new();
+
+    public Dictionary<string, string> ExpectedCallback { get; set; } = new()
+    {
+        {
+            "onSuccess",
+            "The callback URL will receive a POST request with the result upon successful processing."
+        },
+        {
+            "onFailure", "The callback URL will receive a POST request with error details if processing fails."
+        },
+        {
+            "onProgress",
+            "The callback URL may receive periodic POST requests if there are multiple steps involved in the processing."
+        }
+    };
+    
+    public string TrackingId { get; set; } = string.Empty;
 
     public QueueResponse()
     {
     }
-
-    public QueueResponse(
-        string callbackUrl,
-        string statusEndpoint,
-        long estimatedTimeOfCompletion,
-        Dictionary<string, string> expectedCallback)
-    {
-        CallbackUrl = callbackUrl;
-        StatusEndpoint = statusEndpoint;
-        EstimatedTimeOfCompletion = estimatedTimeOfCompletion;
-        ExpectedCallback = expectedCallback;
-    }
-
-    public string TrackingId { get; set; } = string.Empty;
 }
