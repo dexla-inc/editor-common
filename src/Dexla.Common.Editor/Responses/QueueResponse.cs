28d6b368af859abf0956de4fc0a7e452517be759
Tom McDonough <tom@dexla.ai>
2023-11-07 13:29:48 +0000

    Create QueueResponse.cs

---
+++ src/Dexla.Common.Editor/Responses/QueueResponse.cs
@@ -0,0 +1,10 @@
+﻿using Dexla.Common.Types.Interfaces;
+
+namespace Dexla.Common.Editor.Responses;
+
+public class QueueResponse : ISuccess
+{
+    public string Message { get; set; } = "Message processing";
+    public static readonly QueueResponse Instance = new();
+    public string TrackingId { get; set; } = string.Empty;
+}
