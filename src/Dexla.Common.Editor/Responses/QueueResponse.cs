79f3ea2d1c7898bcc7502dd5555cf051fe78ae1c
Tom McDonough <tom@dexla.ai>
2023-11-07 13:46:24 +0000

    Update QueueResponse.cs

--- src/Dexla.Common.Editor/Responses/QueueResponse.cs
+++ src/Dexla.Common.Editor/Responses/QueueResponse.cs
@@ -5,10 +5,14 @@ namespace Dexla.Common.Editor.Responses;
 public class QueueResponse : ISuccess
 {
     public string Message { get; set; } = "Your request has been accepted for processing.";
-    public string CallbackUrl { get; }
-    public string StatusEndpoint { get; }
-    public long EstimatedTimeOfCompletion { get; }
-    public Dictionary<string, string> ExpectedCallback { get; }
+    public string CallbackUrl { get; set; } = string.Empty;
+    public string StatusEndpoint { get; set; } = string.Empty;
+    public long EstimatedTimeOfCompletion { get; set; }
+    public Dictionary<string, string> ExpectedCallback { get; set; } = new();
+
+    public QueueResponse()
+    {
+    }
 
     public QueueResponse(
         string callbackUrl,
