34c9f9542d9184400333d8349a9326a4c7cc84d0
Tom McDonough <tom@dexla.ai>
2023-11-07 16:27:14 +0000

    Update AutomationSettings.cs

--- src/Dexla.Common/Types/AutomationSettings.cs
+++ src/Dexla.Common/Types/AutomationSettings.cs
@@ -7,6 +7,4 @@ public class AutomationSettings
     public string AuthorizationValue { get; set; } = string.Empty;
     public string OrganizationKey { get; set; } = string.Empty;
     public string OrganizationValue { get; set; } = string.Empty;
-    public string Model { get; set; } = string.Empty;
-    public string BackupModel { get; set; } = string.Empty;
 }
