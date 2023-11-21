d71563de72d56b42c684bb9df94f1a01c14ce9b7
Tom McDonough <tom@dexla.ai>
2023-11-21 15:10:48 +0000

    Added blob storage files back in that were deleted
    Upgraded to version 8

---
+++ src/Dexla.Common.BlobStorage/UploadResponse.cs
@@ -0,0 +1,14 @@
+﻿using Dexla.Common.Types.Interfaces;
+
+namespace Dexla.Common.BlobStorage;
+
+public class UploadResponse : ISuccess
+{
+    public string Url { get; set; }
+    public UploadResponse(string url)
+    {
+        Url = url;
+    }
+
+    public string TrackingId { get; set; }
+}
