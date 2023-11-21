ec481eba94118fde6664dab70c240e08becd546e
Tom McDonough <tom@dexla.ai>
2023-11-21 16:39:29 +0000

    Removed UploadMultipleResponse and UploadResponse

--- src/Dexla.Common.BlobStorage/BlobResponse.cs
+++ src/Dexla.Common.BlobStorage/BlobResponse.cs
@@ -12,21 +12,3 @@ public class BlobResponse : ISuccess
 
     public string TrackingId { get; set; }
 }
-
-
-public class ListBlobResponse : ISuccess
-{
-    public List<BlobResponse> Files { get; }
-
-    public ListBlobResponse(List<BlobResponse> files)
-    {
-        Files = files;
-    }
-    
-    public ListBlobResponse(IEnumerable<string> fileNames)
-    {
-        Files = fileNames.Select(fileName => new BlobResponse(fileName)).ToList();
-    }
-
-    public string TrackingId { get; set; }
-}
---
+++ src/Dexla.Common.BlobStorage/ListBlobResponse.cs
@@ -0,0 +1,20 @@
+﻿using Dexla.Common.Types.Interfaces;
+
+namespace Dexla.Common.BlobStorage;
+
+public class ListBlobResponse : ISuccess
+{
+    public List<BlobResponse> Files { get; }
+
+    public ListBlobResponse(List<BlobResponse> files)
+    {
+        Files = files;
+    }
+    
+    public ListBlobResponse(IEnumerable<string> fileNames)
+    {
+        Files = fileNames.Select(fileName => new BlobResponse(fileName)).ToList();
+    }
+
+    public string TrackingId { get; set; }
+}
