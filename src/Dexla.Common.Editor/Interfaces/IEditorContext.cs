be35ab8d13398d9561d66772e11045200f8a876e
Tom McDonough <tom@dexla.ai>
2024-02-01 10:53:58 -0800

    Update IEditorContext.cs

--- src/Dexla.Common.Editor/Interfaces/IEditorContext.cs
+++ src/Dexla.Common.Editor/Interfaces/IEditorContext.cs
@@ -1,20 +1,9 @@
 ﻿using Dexla.Common.Editor.Entities;
 using Dexla.Common.Repository.Types.Interfaces;
-using Dexla.Common.Types;
 
 namespace Dexla.Common.Editor.Interfaces;
 
 public interface IEditorContext : IContext
 { 
     Task<IEnumerable<Deployment>> GetMostRecentEnvironmentDeployments(string projectId);
-
-    Task<TResult> JoinCollections<TEntity, TForeignEntity, TResult>(
-        FilterConfiguration filterConfiguration,
-        string localField,
-        string foreignField,
-        string foreignCollectionName,
-        CancellationToken cancellationToken = default)
-        where TEntity : class, IEntity
-        where TForeignEntity : class, IEntity
-        where TResult : class, IEntity;
 }
