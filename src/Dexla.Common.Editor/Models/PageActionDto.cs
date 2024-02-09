e66322277c59d9825bb05b8d0488481dcf484f33
Tom McDonough <tom@dexla.ai>
2024-02-09 14:05:10 -0800

    Changed action type to string

--- src/Dexla.Common.Editor/Models/PageActionDto.cs
+++ src/Dexla.Common.Editor/Models/PageActionDto.cs
@@ -4,6 +4,6 @@ public class PageActionDto
 {
     public string Id { get; set; } = string.Empty;
     public string Trigger { get; set; }= string.Empty;
-    public object Action { get; set; } = new object();
+    public string Action { get; set; } = string.Empty;
     public string SequentialTo { get; set; }= string.Empty;
 }
