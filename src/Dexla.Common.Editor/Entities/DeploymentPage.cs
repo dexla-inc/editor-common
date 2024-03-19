ab47130758c699fa5dfb172a0095a10f21595e69
Tom McDonough <tom@dexla.ai>
2024-03-19 14:02:17 +0000

    Removed TImestamp from DeploymentPage

--- src/Dexla.Common.Editor/Entities/DeploymentPage.cs
+++ src/Dexla.Common.Editor/Entities/DeploymentPage.cs
@@ -3,15 +3,14 @@ using Dexla.Common.Repository.Types.Interfaces;
 
 namespace Dexla.Common.Editor.Entities;
 
-public class DeploymentPage : IEntity, IEntityWithTimeStamp
+public class DeploymentPage : IEntity
 {
     public string Id { get; set; } = string.Empty;
+    public EntityStatus EntityStatus { get; set; }
     public string DeploymentId { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public virtual bool AuthenticatedOnly { get; set; }
     public virtual string AuthenticatedUserRole { get; set; } = string.Empty;
     public List<string> PageState { get; set; } = [];
-    public EntityStatus EntityStatus { get; set; }
-    public long Timestamp { get; set; }
 }
--- src/Dexla.Common.Editor/Models/DeploymentPageModel.cs
+++ src/Dexla.Common.Editor/Models/DeploymentPageModel.cs
@@ -6,12 +6,11 @@ namespace Dexla.Common.Editor.Models;
 public class DeploymentPageModel : IModel
 {
     public string Id { get; set; } = string.Empty;
+    public EntityStatus EntityStatus { get; set; }
     public string DeploymentId { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public virtual bool AuthenticatedOnly { get; set; }
     public virtual string AuthenticatedUserRole { get; set; } = string.Empty;
     public List<string> PageState { get; set; } = [];
-    public EntityStatus EntityStatus { get; set; }
-    public long Timestamp { get; set; }
 }
