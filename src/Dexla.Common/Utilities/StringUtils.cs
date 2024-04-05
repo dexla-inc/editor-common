60f040df2c2123a730be8abe25fd2fbf1cd4574f
Tom McDonough <tom@dexla.ai>
2024-04-05 14:32:23 +0100

    Fixed ToSnakeCase

--- src/Dexla.Common/Utilities/StringUtils.cs
+++ src/Dexla.Common/Utilities/StringUtils.cs
@@ -1,9 +1,23 @@
-﻿namespace Dexla.Common.Utilities;
+﻿using System.Text.RegularExpressions;
+
+namespace Dexla.Common.Utilities;
 
 public static class StringUtils
 {
     public static string ToSnakeCase(this string str)
     {
-        return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())).ToLower();
+        string temp = str.Replace("_", " ");
+        
+        // Use a regular expression to insert an underscore before any capital letter
+        // that is either preceded by a lowercase letter or a number
+        temp = Regex.Replace(temp, @"((?<=[a-z])[A-Z]|(?<=[A-Z])[A-Z](?=[a-z])|(?<=[A-Za-z])(?=[0-9])|(?<=[0-9])(?=[A-Za-z]))", "_$1");
+
+        // Replace multiple spaces with a single space
+        temp = Regex.Replace(temp, @"\s+", " ");
+        
+        // Trim spaces at the start and end, and replace remaining spaces with underscores
+        temp = temp.Trim().Replace(" ", "_");
+
+        return temp.ToLower();
     }
 }
