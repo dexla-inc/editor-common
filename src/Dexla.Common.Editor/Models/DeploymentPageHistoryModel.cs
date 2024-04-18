9728ea8ffef78c2bae8f987b3fbd6fefdc7cf60e
Tom McDonough <tom@dexla.ai>
2024-04-18 10:56:41 +0100

    Added pageId and created to deployment page history response

--- src/Dexla.Common.Editor/Models/DeploymentPageHistoryModel.cs
+++ src/Dexla.Common.Editor/Models/DeploymentPageHistoryModel.cs
@@ -10,9 +10,11 @@ public class DeploymentPageHistoryModel : IModel
     public string ProjectId { get; set; } = string.Empty;
     public string UserId { get; set; } = string.Empty;
     public string DeploymentId { get; set; } = string.Empty;
+    public string PageId { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public bool AuthenticatedOnly { get; set; }
     public string AuthenticatedUserRole { get; set; } = string.Empty;
     public List<string> PageState { get; set; } = [];
+    public long Created { get; set; }
 }
--- src/Dexla.Common.Editor/Responses/DeploymentPageHistoryResponse.cs
+++ src/Dexla.Common.Editor/Responses/DeploymentPageHistoryResponse.cs
@@ -10,21 +10,27 @@ public class DeploymentPageHistoryResponse : ISuccess
     public bool AuthenticatedOnly { get; }
     public string AuthenticatedUserRole { get; }
     public string? PageState { get; set; }
+    public string PageId { get; }
+    public long Created { get; }
 
     public DeploymentPageHistoryResponse(
         string id,
+        string pageId,
         string title,
         string slug,
         bool authenticatedOnly,
         string authenticatedUserRole,
-        IEnumerable<string> pageState)
+        IEnumerable<string> pageState,
+        long created)
     {
         Id = id;
+        PageId = pageId;
         Title = title;
         Slug = slug;
         AuthenticatedOnly = authenticatedOnly;
         AuthenticatedUserRole = authenticatedUserRole;
         PageState = string.Join("", pageState);
+        Created = created;
     }
 
 
