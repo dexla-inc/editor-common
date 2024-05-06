df6ed48e89a53b593f1130500586976fb25eec0d
Tom McDonough <tom@dexla.ai>
2024-05-06 18:54:41 +0100

    Instantiated redirects with string default value

--- src/Dexla.Common.Editor/Entities/Redirects.cs
+++ src/Dexla.Common.Editor/Entities/Redirects.cs
@@ -2,6 +2,6 @@ namespace Dexla.Common.Editor.Entities;
 
 public class Redirects
 {
-    public string SignInPageId { get; set; }
-    public string NotFoundPageId { get; set; }
+    public string SignInPageId { get; set; } = string.Empty;
+    public string NotFoundPageId { get; set; } = string.Empty;
 }
